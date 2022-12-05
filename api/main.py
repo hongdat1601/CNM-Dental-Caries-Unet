192df5337f59344ffb5799002f1c0fe3459d6070
hongdat1601 <hongdat1601@gmail.com>
2022-12-05 13:33:22 +0700

    add api

---
+++ api/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, UploadFile
+from PIL import Image
+from io import BytesIO
+import base64
+
+from model import create_model
+from utils import predict_img, overlay_img
+
+model = create_model()
+model.load_weights("./weights/weights_03-12-22-510637.hdf5")
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"msg": "Hello World"}
+
+@app.post("/predict")
+def predict(file: UploadFile):
+    img = Image.open(file.file)
+    pred = predict_img(model, img)
+
+    im_res = BytesIO()
+    img = Image.fromarray(overlay_img(img, pred))
+    img.save(im_res, "JPEG")
+
+    res = base64.b64encode(im_res.getvalue())
+
+    return {"data": res}
